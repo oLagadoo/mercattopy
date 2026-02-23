0d532a2f5593e5908abcad458b9ada6697aaf439
oLagadoo <fvcklagadoo@gmail.com>
2026-02-22 21:55:35 -0300

    feat: adiciona create, update, delete de Produto com ModelForm e validações

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,9 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
     path('', views.dashboard, name='dashboard'),
     path('categorias/', views.category_list, name='category_list'),
     path('produtos/', views.product_list, name='product_list'),
+    path('produtos/novo/', views.product_create, name='product_create'),
+    path('produtos/<int:pk>/editar/', views.product_update, name='product_update'),
+    path('produtos/<int:pk>/excluir/', views.product_delete, name='product_delete'),
     path('produtos/<int:pk>/', views.product_detail, name='product_detail'),
     path('logout/', views.logout_view, name='logout'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,10 @@
+from .forms import ProductForm
+from django.shortcuts import redirect
+from django.contrib import messages
 from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
 from django.shortcuts import get_object_or_404
+
 
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
@@ -26,3 +30,51 @@ def dashboard(request):
 
 def logout_view(request):
     return render(request, 'auth/login.html')
+
+def product_create(request):
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Produto criado com sucesso!")
+            return redirect('product_list')
+
+    else:
+        form = ProductForm()
+
+    return render(request, 'catalog/product_form.html', {'form': form})
+
+
+def product_delete(request, pk):
+
+    product = get_object_or_404(Product, pk=pk)
+
+    if request.method == 'POST':
+
+        # 🔥 REGRA DE NEGÓCIO
+        if product.stock > 0:
+            messages.error(request, "Não é possível excluir produto com estoque maior que zero.")
+            return redirect('product_list')
+
+        product.delete()
+        messages.success(request, "Produto excluído com sucesso!")
+        return redirect('product_list')
+
+    return render(request, 'catalog/product_confirm_delete.html', {'product': product})
+
+
+def product_update(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Produto atualizado com sucesso!')
+            return redirect('product_list')
+    else:
+        form = ProductForm(instance=product)
+
+    return render(request, 'catalog/product_form.html', {'form': form})
